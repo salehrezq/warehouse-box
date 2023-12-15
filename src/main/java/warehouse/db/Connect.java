57204941b8a554c7dc9f7d7c1af35ce19f639212
salehrezq <salehrezq@gmail.com>
2023-12-15 10:01:38 +0300

    Implement database connectivity class `Connect`

--- src/main/java/warehouse/db/Connect.java
+++ src/main/java/warehouse/db/Connect.java
@@ -23,10 +23,77 @@
  */
 package warehouse.db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Saleh
  */
 public class Connect {
 
+    // JDBC driver name and database URL
+    private static final String JDBC_DRIVER = "org.mariadb.jdbc.Driver";
+    private static final String DB_URL = "jdbc:mariadb://localhost:3306/warehouse_box_db";
+
+    //  Database credentials
+    private static final String USER = "root";
+    private static final String PASS = "1212";
+
+    // For atomic statements
+    private static Connection conn;
+
+    public static Connection getConnection() {
+        cleanUp();
+        conn = null;
+        try {
+            Class.forName(JDBC_DRIVER);
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+            conn.setAutoCommit(false);
+        } catch (SQLException | ClassNotFoundException ex) {
+            Logger.getLogger(Connect.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return conn;
+    }
+
+    public static Connection getConnection(boolean usePreviousConnectionForAtomicCommit) {
+
+        if (conn != null && usePreviousConnectionForAtomicCommit) {
+            return conn;
+        }
+
+        conn = null;
+
+        try {
+            Class.forName(JDBC_DRIVER);
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+            conn.setAutoCommit(false);
+        } catch (SQLException | ClassNotFoundException ex) {
+            Logger.getLogger(Connect.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return conn;
+    }
+
+    public static void rollBack() {
+        if (conn != null) {
+            try {
+                conn.rollback();
+            } catch (SQLException ex) {
+                Logger.getLogger(Connect.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+
+    public static void cleanUp() {
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(Connect.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
 }
