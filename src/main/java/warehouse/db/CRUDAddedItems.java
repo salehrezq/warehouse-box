3e18bddf20b29dba4b38633d37caa9434330891f
salehrezq <salehrezq@gmail.com>
2024-03-17 16:59:26 +0300

    Update mysql selection of added items to order

--- src/main/java/warehouse/db/CRUDAddedItems.java
+++ src/main/java/warehouse/db/CRUDAddedItems.java
@@ -79,7 +79,7 @@ public class CRUDAddedItems {
                     + " ad.date, i.name AS item_name, i.specification AS item_specs"
                     + " FROM added_items as ad JOIN items AS i JOIN quantity_unit AS u JOIN source AS s"
                     + " ON (ad.item_id = i.id) AND (i.unit_id = u.id) AND (s.id = ad.source_id)"
-                    + " ORDER BY ad.date ASC;";
+                    + " ORDER BY ad.date ASC, ad.id ASC;";
 
             con = Connect.getConnection();
             PreparedStatement p;
