5ae0e5ac114b0d97981372f59db63ab574db0a4e
salehrezq <salehrezq@gmail.com>
2025-03-08 09:11:01 +0300

    Implement the `SearchFormatter` class

--- src/main/java/warehousebox/db/SearchFormatter.java
+++ src/main/java/warehousebox/db/SearchFormatter.java
@@ -1,9 +1,32 @@
 package warehousebox.db;
 
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 /**
  *
  * @author Saleh
  */
 public class SearchFormatter {
 
+    protected static String trimExtraSpaces(String text) {
+        return text.trim().replaceAll("\\s+", " ");
+    }
+
+    protected static String[] getUniqueArrayOfWords(String[] words) {
+        ArrayList<String> uniqueWords = new ArrayList<>();
+        Set<String> set = new LinkedHashSet<>();
+
+        for (String word : words) {
+            if (set.add(word)) {
+                uniqueWords.add(word);
+            }
+        }
+        return uniqueWords.toArray(String[]::new);
+    }
+
+    protected static String[] getArrayOfWords(String text) {
+        return getUniqueArrayOfWords(trimExtraSpaces(text).toLowerCase().split("\\W+"));
+    }
 }
